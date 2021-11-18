13532b3c4a5ddf31997c76de2b5779dc3186d210
YoungMayor <youngmayor08@gmail.com>
2021-11-18 03:50:24 +0100

    Built the package

--- src/WebServiceServiceProvider.php
+++ src/WebServiceServiceProvider.php
@@ -20,9 +20,9 @@ class WebServiceServiceProvider extends ServiceProvider
         // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/config.php' => config_path('web-service.php'),
-            ], 'config');
+            // $this->publishes([
+            //     __DIR__ . '/../config/config.php' => config_path('web-service.php'),
+            // ], 'config');
 
             // Publishing the views.
             /*$this->publishes([
